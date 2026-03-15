b3e3a5a0e2360488c047fb0200d7a39554a6f840
Krithika-alt <krithika12907@gmail.com>
2026-03-15 15:27:31 -0500

    Added CircadianData model with heart rate and brain fog fields

--- src/com/neuronod/backend/CircadianData.java
+++ src/com/neuronod/backend/CircadianData.java
@@ -1,4 +1,26 @@
 package com.neuronod.backend;
 
+
 public class CircadianData {
+    private long time;
+    private double heartRate;
+    private int brainFog;
+
+    public CircadianData(long time, double heartRate, int brainFog){
+        this.time = time;
+        this.heartRate = heartRate;
+        this.brainFog = brainFog;
+    }
+
+    public double getHeartRate(){
+        return heartRate;
+    }
+
+    public long getTime(){
+        return time;
+    }
+
+    public int getBrainFog(){
+        return brainFog;
+    }
 }
