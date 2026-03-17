88d95f908f38cb9fbf0b7793067137b9085797f9
Krithika-alt <krithika12907@gmail.com>
2026-03-16 19:28:57 -0500

    Added logic to filter outliers in data and smoothed data by calculating the moving average

--- src/com/neuronod/backend/CircadianMath.java
+++ src/com/neuronod/backend/CircadianMath.java
@@ -3,6 +3,7 @@ package com.neuronod.backend;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CircadianMath {
@@ -36,5 +37,51 @@ public class CircadianMath {
         String formattedEnd = Instant.ofEpochMilli(endTime).atZone(ZoneId.systemDefault()).format(formatter);
 
         return formattedStart + " - " + formattedEnd;
+    }
+
+    public List<CircadianData> filterOutliers(List<CircadianData> rawData){
+        List<CircadianData> cleanedData = new ArrayList<>();
+        for (int i = 0; i < rawData.size(); ++i){
+            if ((rawData.get(i).getHeartRate() > 30) && (rawData.get(i).getHeartRate() < 200)){
+                cleanedData.add(rawData.get(i));
+            }
+        }
+
+        return cleanedData;
+    }
+
+    public List<CircadianData> calculateMovingAverage(List<CircadianData> cleanData, int windowSize){
+        List<CircadianData> smoothedList = new ArrayList<>();
+        for (int i = 0; i < cleanData.size(); ++i){
+            double sum = 0;
+            int count = 0;
+            double averageHR = 0;
+            int ogBrainFog = 0;
+            long ogTime = 0;
+
+            int start = Math.max(0, i - windowSize);
+            int end = Math.min(cleanData.size() - 1, i + windowSize);
+
+            for (int j = start; j <= end; j++){
+                sum += cleanData.get(j).getHeartRate();
+                count++;
+            }
+
+            averageHR = sum/count;
+            ogBrainFog = cleanData.get(i).getBrainFog();
+            ogTime = cleanData.get(i).getTime();
+
+            smoothedList.add(new CircadianData(ogTime, averageHR, ogBrainFog));
+        }
+
+        return smoothedList;
+    }
+
+    public String getHealingWindow(List<CircadianData> rawData){
+        List<CircadianData> cleaned = filterOutliers(rawData);
+        List<CircadianData> smoothed = calculateMovingAverage(cleaned, 5);
+        long nadirTime = findNadirTime(smoothed);
+
+        return calculateHealingWindow(nadirTime);
     }
 }
